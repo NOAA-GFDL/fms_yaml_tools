eb98f8d86cd351cf80388a0239ac6a3c07d920eb
mlee03 <Mikyung.Lee@noaa.gov>
2021-05-25 07:54:07 -0400

    parse all draft

--- table_to_yaml.py
+++ table_to_yaml.py
@@ -19,6 +19,8 @@
 # field section
 # "module_name", "field_name", "output_name", "file_name", "time_sampling", "reduction_method", "regional_section", packing
 
+import copy as cp
+
 
 # global section
 class DiagTable :
@@ -36,47 +38,82 @@ class DiagTable :
                                {'base_minute':'' },
                                {'base_second': ''} ]
 
-        self.file_section = [{'file_name'          : ''},
-                             {'output_freq'        : ''},
-                             {'output_freq_units'  : ''},
-                             {'file_format'        : ''},
-                             {'time_axis_units'    : ''},
-                             {'time_axis_name'     : ''},
-                             {'new_file_freq'      : ''},
-                             {'new_file_freq_units': ''},
-                             {'start_time'         : ''},
-                             {'file_duration'      : ''},
-                             {'file_duration_units': ''},
-                             {'filename_suffix'    : ''} ]
+        self.file_section_tmp = [{'file_name'          : ''},
+                                 {'output_freq'        : ''},
+                                 {'output_freq_units'  : ''},
+                                 {'file_format'        : ''},
+                                 {'time_axis_units'    : ''},
+                                 {'time_axis_name'     : ''},
+                                 {'new_file_freq'      : ''},
+                                 {'new_file_freq_units': ''},
+                                 {'start_time'         : ''},
+                                 {'file_duration'      : ''},
+                                 {'file_duration_units': ''},
+                                 {'filename_suffix'    : ''} ]
+        self.file_section = []
 
-        self.field_section = [{ 'module_name'     : ''},
-                              { 'field_name'      : ''},
-                              { 'output_name'     : ''},
-                              { 'file_name'       : ''},
-                              { 'time_sampling'   : ''},
-                              { 'reduction_method': ''},
-                              { 'regional_section': ''},
-                              { 'packing'         : ''} ]
+        self.field_section_tmp = [{'module_name'     : ''},
+                                  {'field_name'      : ''},
+                                  {'output_name'     : ''},
+                                  {'file_name'       : ''},
+                                  {'time_sampling'   : ''},
+                                  {'reduction_method': ''},
+                                  {'regional_section': ''},
+                                  {'packing'         : ''} ]
+        self.field_section = []
+
         self.diag_table_content = []
 
         #: check if diag_table file exists
         #if self.diag_table_filename
 
-    def read_diag_table_filename(self) :
+    def read_diag_table(self) :
         with open( self.diag_table_filename, 'r' ) as myfile :
             for iline in myfile.readlines() :
-                if iline.strip() != '' : self.diag_table_content.append( iline.strip() )
+                if iline.strip() != '' and '#' not in iline : self.diag_table_content.append( iline.strip() )
 
     def parse_global_section(self) :
         if self.diag_table_content == [] : print( 'something is wrong' )
         self.global_section[0]['title'] = self.diag_table_content[0]
-        line_two_list = self.diag_table_content[1].split(' ')
+        line_two_list = self.diag_table_content[1].strip().split(' ')
         for i in range(len(line_two_list)) :
             ikey = self.global_section[i+1].keys()[0]
             self.global_section[i+1][ikey] = int(line_two_list[i])
 
+    def parse_file_section(self) :
+        if self.diag_table_content == [] : print( 'something is wrong' )
+        for iline in self.diag_table_content[2:] :
+            if 'mod' not in iline :
+                iline_list = iline.strip().split(',')
+                for i in range(len(iline_list)) :
+                    ikey = self.file_section_tmp[i].keys()[0]
+                    self.file_section_tmp[i][ikey] = iline_list[i]
+            self.file_section.append( cp.deepcopy(self.file_section_tmp) )
+
+    def parse_field_section(self) :
+        if self.diag_table_content == [] : print( 'something is wrong' )
+        for iline in self.diag_table_content[2:] :
+            if 'mod' in iline :
+                iline_list = iline.strip().split(',')
+                for i in range(len(iline_list)) :
+                    ikey = self.field_section_tmp[i].keys()[0]
+                    self.field_section_tmp[i][ikey] = iline_list[i]
+                self.field_section.append( cp.deepcopy(self.field_section_tmp) )
+
+    def parse_all_sections(self) :
+        if self.diag_table_content == [] : print( 'something is wrong' )
+        self.parse_global_section()
+        self.parse_file_section()
+        self.parse_field_section()
+
+    def read_and_parse_all_sections(self) :
+        self.read_diag_table()
+        self.parse_all_sections()
+
 
 test_class = DiagTable( diag_table_filename='diag_table_21' )
-test_class.read_diag_table_filename()
+test_class.read_diag_table()
 test_class.parse_global_section()
-print( test_class.global_section )
+test_class.parse_file_section()
+test_class.parse_field_section()
+print( test_class.field_section )
