e8f153aed44f612f8cfda17baf1d4680d4a5b3e1
mlee03 <Mikyung.Lee@noaa.gov>
2021-05-28 09:45:03 -0400

    add data_table_to_yaml draft 2

--- data_table_to_yaml.py
+++ data_table_to_yaml.py
@@ -3,6 +3,24 @@
 import copy as cp
 
 
+#: write '---' at top of the yaml file
+def init_yaml_file(outfile='') :
+    with open(outfile,'w') as myfile : myfile.write('---\n')
+
+
+#: section = [ [list1], [list2], [list3], ... ]
+def write_yaml_sections(outfile='', section=[], header='') :
+    with open(outfile, 'a+') as myfile :
+        myfile.write( header + ':\n')
+        for ilist in section :
+            mystr = ' {:2s}' + '{:17s} : ' + '{:' + str(len(ilist[0].values())) + 's} \n'
+            myfile.write( mystr.format( '-', str(*ilist[0].keys()) , str(*ilist[0].values()) ))
+            for i in range(1,len(ilist)) :
+                mystr = ' {:2s}' + '{:17s} : ' + '{:' + str(len(ilist[i].values())) + 's} \n'
+                myfile.write( mystr.format( '', str(*ilist[i].keys()) , str(*ilist[i].values()) ))
+            myfile.write('\n')
+
+
 class DataType :
 
     def __init__(self, data_table_file='data_table') :
@@ -38,20 +56,21 @@ class DataType :
 
     def read_data_table(self) :
         if self.data_table_content != [] : self.data_table_content = []
+        iline_count = 0
         with open(self.data_table_file, 'r') as myfile :
-            iline_count = 0
             for iline in myfile.readlines() :
                 iline_count += 1
                 if iline.strip() != '' and '#' not in iline.strip()[0] :
-                    iline_list = iline.strip().split(',')
+                    iline_list = iline.split(',')
                     for i in range( len(iline_list) ) :
                         try :
                             myfunct= self.data_type_values[i][self.data_type_keys[i]]
                             iline_list[i] = myfunct( iline_list[i].strip() )
                         except :
-                            exit( '\nERROR in line ' + str(iline_count) + ": " + iline + 'CHECK '
-                                  + str(iline_list[i]) )
+                            exit( '\nERROR in line ' + str(iline_count) + ": " + iline +
+                                  '\nCHECK element ' + str(iline_list[i]) )
                 self.data_table_content.append( cp.deepcopy(iline_list) )
+
 
     def parse_data_table(self) :
         if self.data_table_content == [] : exit('somethings wrong')
@@ -60,7 +79,20 @@ class DataType :
             self.data_type.append( cp.deepcopy(tmp_list) )
 
 
+    def read_and_parse_data_table(self) :
+        if self.data_table_content != [] : self.data_table_content = []
+        self.read_data_table()
+        self.parse_data_table()
+
+
+    def write_yaml( self ) :
+        outfile = self.data_table_file + '.yaml'
+        init_yaml_file(outfile)
+        write_yaml_sections( outfile, self.data_type, header='data_table' )
+
+
+
 test_class = DataType(data_table_file='data_table')
-test_class.read_data_table()
-test_class.parse_data_table()
+test_class.read_and_parse_data_table()
+test_class.write_yaml()
 print( test_class.data_type )
