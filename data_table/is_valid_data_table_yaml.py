f67f5f8107d8e249fd9f636b6dc867a10aaa9751
Uriel Ramirez <uriel.ramirez@noaa.gov>
2022-05-31 12:50:07 -0400

    Adds more documentation to the is_valid_* and the data_table_to_yaml scripts. Updates the diag_table converter to support the corners, fixes some bugs in the diag_table converter

--- data_table/is_valid_data_table_yaml.py
+++ data_table/is_valid_data_table_yaml.py
@@ -1,8 +1,23 @@
+#!/usr/bin/env python3
+""" Determine if a yaml data_table is valid.
+    Run `python3 is_valid_data_table_yaml.py -h` for more details
+    Author: Uriel Ramirez 05/27/2022
+"""
+
 import yaml
 import sys
 import argparse
 
+parser = argparse.ArgumentParser(prog='is_valid_data_table_yaml', \
+                                description="Determines if a yaml data_table is valid. \
+                                             Requires pyyaml (https://pyyaml.org/) \
+                                             More details on the yaml format can be found in \
+                                             https://github.com/NOAA-GFDL/FMS/tree/main/data_override")
+parser.add_argument('-f', type=str, help='Name of the data_table yaml to check' )
+in_data_table = parser.parse_args().f
+
 def check_gridname(grid_name):
+  """"Check if the input grid_name is valid. Crashes if it not.""""
     valid = ["OCN", "LND", "ATM", "ICE"]
     if (gridname not in valid): raise Exception(grid_name+ ' is not a valid gridname. The only values allowed are "OCN", "LND", "ATM", "ICE"')
 
@@ -13,26 +28,25 @@ def check_filename_and_field(field, interp_method):
     if (field =="" and interp_method != ""): raise Exception('If "fieldname_file" is empty, interp_method must be empty')
 
 def check_interp_method(interp_method):
+  """"Check if the interp method is valid. Crashes if it not. """"
     valid = ["bilinear", "bicubic", "none"]
     if (interp_method not in valid): raise Exception(interp_method + ' is not a valid interp_method. The only values allowed are "bilinear", "bicubic", and "none"')
 
 def check_region_type(region_type):
+  """"Check if the input region type is valid. Crashes if it is not.""""
     valid = ["inside_region", "outside_region"]
     if (region_type not in valid): raise Exception(region_type + 'is not a valid region_type. The only values allowed are "inside_region" and "outside_region"')
 
 def check_if_bounds_present(entry):
+  """"Check if the region bounds are valid, crashes if they are not """"
     if ("lat_start" not in entry): raise Exception('lat_start must be present if region_type is set')
     if ("lat_end" not in entry): raise Exception('lat_end must be present if region_type is set')
     if ("lon_start" not in entry): raise Exception('lon_start must be present if region_type is set')
     if ("lon_end" not in entry): raise Exception('lon_end must be present if region_type is set')
 
 def check_region(my_type, start, end):
+  """"Check if the region is defined correctly. Crashes if it not. """"
     if (start > end): raise Exception(my_type+"_start is greater than "+my_type+"_end")
-
-#: parse user input
-parser = argparse.ArgumentParser(prog='is_valid_data_table_yaml', description="Determines if a yaml data_table is valid. More details on the yaml format can be found in https://github.com/NOAA-GFDL/FMS/tree/main/data_override")
-parser.add_argument('-f', type=str, help='Name of the data_table yaml to check' )
-in_data_table = parser.parse_args().f
 
 with open(in_data_table) as fl:
     my_table = yaml.safe_load(fl)
