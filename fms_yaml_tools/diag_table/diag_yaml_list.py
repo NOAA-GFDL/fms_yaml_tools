0e2823405b2e6561550cb9d6986ba78edcf5de7a
Thomas Robinson <Thomas.Robinson@noaa.gov>
2023-12-15 09:19:28 -0500

    Adds more options and prints more information in diag_yaml_list.py

--- fms_yaml_tools/diag_table/diag_yaml_list.py
+++ fms_yaml_tools/diag_table/diag_yaml_list.py
@@ -28,42 +28,50 @@
 import click
 import yaml
 import sys
+import copy
 
 @click.command()
-@click.option('--print_vars/--no-print_vars', type=click.BOOL, show_default=True, default=False,
+@click.option('--fileinfo/--no-fileinfo', type=click.BOOL, show_default=True, default=False,
+  help="Print all of the file information. Default only prints history file names")
+@click.option('--varlist/--no-varlist', type=click.BOOL, show_default=True, default=False,
   help="Prints the list of variables and the module they are from below each history file")
 @click.option('--comma/--no-comma', type=click.BOOL, show_default=True, default=False,
   help="With --print_vars gives a comma separated list of the variables instead of a vertical listing")
 @click.argument("table")
-def dyl(table, print_vars, comma):
+def dyl(table, fileinfo, varlist, comma):
   """ Lists the history file names in a diag table YAML
 
       TABLE is the path of the diag table YAML file
   """
   with open(table) as fl:
     my_table = yaml.safe_load(fl)
-    print_diag_file(my_table, print_vars=print_vars, comma=comma)
+    print_diag_file(my_table, fileinfo, print_vars=varlist, comma=comma)
 
 """ Prints the varlist for the given diag_file """
 def print_diag_file_vars(my_table, diag_file, comma=False):
   if "varlist" in my_table["diag_files"][diag_file]:
     if not comma: ## The default is one variable per line
-      for i in range(len(my_table["diag_files"][diag_file]["varlist"])):
-        print("  -",my_table["diag_files"][diag_file]["varlist"][i]["var_name"]+" from module "+ \
-             my_table["diag_files"][diag_file]["varlist"][i]["module"])
+      for var in my_table["diag_files"][diag_file]["varlist"]:
+        print(" - ",var)
     else:
       varlist = []
       for v in my_table["diag_files"][diag_file]["varlist"]:
         varlist.append(v["var_name"])
-      print(varlist)
+      print(" - ",varlist)
   else:
       print(" ("+my_table['diag_files'][diag_file]["file_name"]+" has no varlist)")
 
 """ Prints the diag_files in a diag_table.yaml """
-def print_diag_file(my_table, print_vars=False, comma=False):
-  for i in range(len(my_table["diag_files"])):
-    print("file name:",my_table["diag_files"][i]["file_name"])
-    if print_vars:
+def print_diag_file(my_table, file_info, print_vars=False, comma=False):
+  for f in my_table["diag_files"]: #loop through files
+    if file_info: #Print all file info
+      fonly = copy.deepcopy(f) #Create a copy and pop off the varlist
+      fonly.pop("varlist", None)
+      print ("FILE: ",fonly) #Print file information without the varlist
+    else: #Print only the file name
+      print (f["file_name"])
+    if print_vars: #Print variable list/info
+      i = my_table["diag_files"].index(f)
       print_diag_file_vars(my_table, i, comma)
 
 if __name__ == '__main__':
