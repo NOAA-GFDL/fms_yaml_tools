51985d01056df523b8819ad1531c544c16dd4c7d
Thomas Robinson <Thomas.Robinson@noaa.gov>
2024-05-09 07:14:08 -0400

    Fixes typo
    Improves documentation for validate_schema.py

--- fms_yaml_tools/schema/validate_schema.py
+++ fms_yaml_tools/schema/validate_schema.py
@@ -3,21 +3,32 @@ import click
 import yaml
 import json
 from jsonschema import validate, ValidationError, SchemaError
-
+""" This program is used for validating a file against a schema.  The
+schema is written in JSON format, so the file it is validating must be
+in a JSON-like format.  That means that this can be used to validate JSON
+or YAML files, but the YAML files must adhere to some of the JSON rules.
+The arguments of this program are the path to the file to be validated
+which is ypath and the path to the schema file which is spath.  
+This program uses click for accepting command like arguments and options
+and jsonschema to run the validation.
+"""
 @click.command()
+# Debug is used to print more information to the screen.
 @click.option('--debug/--no-debug', type=click.BOOL, show_default=True, default=False,
    help="Print steps in the validation")
+# This option controls whether or not the program prints a success message
 @click.option('--success/--no-show-success', type=click.BOOL, show_default=True, default=False,
    help="Print success message")
-@click.argument("ypath")
-@click.argument("spath")
+@click.argument("ypath") # This is the path to the file to be validated
+@click.argument("spath") # This is the path to the schema to use for validation
 
-def valyaml (ypath,spath,debug,success):
+def valyaml (ypath,spath,debug,success): #main program
   """ Validates a file (YAML or JSON) based on a schema. \n
       YPATH - Path to the YAML file to be validated against the schema \n
       SPATH - Path to the schema file to use for validation \n
 
   """
+# The debug messages are basically comments showing what the code is doing
   if debug:
     print ("Open "+ypath)
   with open(ypath, 'r') as file:
