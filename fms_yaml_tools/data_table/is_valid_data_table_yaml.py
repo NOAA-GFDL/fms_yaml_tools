03e1bf1e98f38f6b77ef32664495cf0c8a671384
Uriel Ramirez <uramirez8707@gmail.com>
2024-07-11 10:25:24 -0400

    call validate_yaml in is_valid_data_table_yaml to use the schema to validate it

--- fms_yaml_tools/data_table/is_valid_data_table_yaml.py
+++ fms_yaml_tools/data_table/is_valid_data_table_yaml.py
@@ -28,38 +28,9 @@
 import yaml
 import sys
 import argparse
+import pkg_resources
 
-def check_gridname(grid_name):
-    """Check if the input grid_name is valid. Crashes if it not."""
-    valid = ["OCN", "LND", "ATM", "ICE"]
-    if (grid_name not in valid): raise Exception(grid_name+ ' is not a valid gridname. The only values allowed are "OCN", "LND", "ATM", "ICE"')
-
-def check_fieldname_code(fieldname):
-    if (fieldname == ""): raise Exception("Fieldname can't be empty")
-
-def check_filename_and_field(field, interp_method):
-    if (field =="" and interp_method != ""): raise Exception('If "fieldname_file" is empty, interp_method must be empty')
-
-def check_interp_method(interp_method):
-    """Check if the interp method is valid. Crashes if it not. """
-    valid = ["bilinear", "bicubic", "none"]
-    if (interp_method not in valid): raise Exception(interp_method + ' is not a valid interp_method. The only values allowed are "bilinear", "bicubic", and "none"')
-
-def check_region_type(region_type):
-    """Check if the input region type is valid. Crashes if it is not."""
-    valid = ["inside_region", "outside_region"]
-    if (region_type not in valid): raise Exception(region_type + 'is not a valid region_type. The only values allowed are "inside_region" and "outside_region"')
-
-def check_if_bounds_present(entry):
-    """Check if the region bounds are valid, crashes if they are not """
-    if ("lat_start" not in entry): raise Exception('lat_start must be present if region_type is set')
-    if ("lat_end" not in entry): raise Exception('lat_end must be present if region_type is set')
-    if ("lon_start" not in entry): raise Exception('lon_start must be present if region_type is set')
-    if ("lon_end" not in entry): raise Exception('lon_end must be present if region_type is set')
-
-def check_region(my_type, start, end):
-    """Check if the region is defined correctly. Crashes if it not. """
-    if (start > end): raise Exception(my_type+"_start is greater than "+my_type+"_end")
+from fms_yaml_tools.schema.validate_schema import validate_yaml
 
 def main():
     parser = argparse.ArgumentParser(prog='is_valid_data_table_yaml', \
@@ -70,51 +41,8 @@ def main():
     parser.add_argument('-f', type=str, help='Name of the data_table yaml to check' )
     in_data_table = parser.parse_args().f
 
-    with open(in_data_table) as fl:
-        my_table = yaml.safe_load(fl)
-        for key, value in my_table.items():
-            for i in range(0, len(value)):
-                entry = value[i]
-                if "gridname" not in entry:
-                    raise Exception("gridname is a required key!")
-                gridname = entry["gridname"]
-                check_gridname(gridname)
-
-                if "fieldname_code" not in entry:
-                    raise Exception("fieldname_code is a required key!")
-
-                fieldname_code = entry["fieldname_code"]
-                check_fieldname_code(fieldname_code)
-
-                if "fieldname_file" in entry:
-                    fieldname_file = entry["fieldname_file"]
-
-                if "file_name" in entry:
-                    file_name = entry["file_name"]
-
-                if "interpol_method" in entry:
-                    interp_method = entry["interpol_method"]
-
-                check_interp_method(interp_method)
-                check_filename_and_field(fieldname_file, interp_method)
-
-                if "factor" not in entry:
-                    raise Exception("factor is a required key")
-
-                factor = entry["factor"]
-
-                if "region_type" in entry:
-                    region_type = entry["region_type"]
-                    check_region_type(region_type)
-                    check_if_bounds_present(entry)
-
-                    lat_start = entry["lat_start"]
-                    lat_end = entry["lat_end"]
-                    check_region("lat", lat_start, lat_end)
-
-                    lon_start = entry["lon_start"]
-                    lon_end = entry["lon_end"]
-                    check_region("lon", lon_start, lon_end)
+    data_path = pkg_resources.resource_filename('fms_yaml_tools', '')
+    validate_yaml(in_data_table, data_path + "/schema/gfdl_msd_schemas/FMS/data_table.json", False, False)
 
 if __name__ == "__main__":
     main()
