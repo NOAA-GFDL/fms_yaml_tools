9707dbabd6cde5efc5bbd3e79ff68fe937346167
uramirez8707 <49168881+uramirez8707@users.noreply.github.com>
2024-07-29 10:37:58 -0400

    Modify the data yaml format to the new format (#48)
    * Modify the yaml format to the new format
    
    * Fix unit test and make converter flake8 compliant
    
    * Remove trailing whitespace
    
    * Fix the combiner after the data_table update
    
    * move data_table_to_yaml to click, add a debug option
    
    * add click to the requirements
    
    * move combine_data_table_yaml to click
    
    * Add a debug option to combine_data_table_yamls
    
    * fix flake8 issue

--- fms_yaml_tools/data_table/data_table_to_yaml.py
+++ fms_yaml_tools/data_table/data_table_to_yaml.py
@@ -21,51 +21,69 @@
 
 from os import path, strerror
 import errno
-import argparse
 import yaml
+import click
 from .. import __version__, TableParseError
 
 
-""" Converts a legacy ascii data_table to a yaml data_table.
-    Run `python3 data_table_to_yaml.py -h` for more details
-    Author: Uriel Ramirez 05/27/2022
-"""
+@click.command()
+# Debug is used to print more information to the screen.
+@click.option('--debug/--no-debug', type=click.BOOL, show_default=True, default=False,
+              help="Print steps in the conversion")
+@click.option('--output-yaml',  type=click.STRING, show_default=True, default="data_table.yaml",
+              help="Path to the output data yable yaml")
+@click.option('--force-write/--no-force-write', type=click.BOOL, show_default=True, default=False,
+              help="Overwrite the output yaml file if it already exists")
+@click.version_option(__version__, "--version")
+@click.argument("data-table-name")  # This is the path to the data_table to convert
+def data_to_yaml(data_table_name, debug, output_yaml, force_write):
+    """ Converts a legacy ascii data_table to a yaml. \n
+        data-table-name - data to the field table to convert \n
+    """
+    try:
+        test_class = DataType(data_table_file=data_table_name,
+                              yaml_table_file=output_yaml,
+                              force_write=force_write, debug=debug)
+        test_class.convert_data_table()
+    except Exception as err:
+        raise SystemExit(err)
 
 
 class DataType:
     def __init__(self, data_table_file='data_table',
                  yaml_table_file='data_table.yaml',
-                 force_write=False):
+                 force_write=False, debug=False):
         """Initialize the DataType"""
         self.data_table_file = data_table_file
         self.yaml_table_file = yaml_table_file
+        self.verboseprint = print if debug else lambda *a, **k: None
         self.out_file_op = "x"  # Exclusive write
         if force_write:
             self.out_file_op = "w"
 
         self.data_type = {}
-        self.data_type_keys = ['gridname',
-                               'fieldname_code',
-                               'fieldname_file',
+        self.data_type_keys = ['grid_name',
+                               'fieldname_in_model',
+                               'fieldname_in_file',
                                'file_name',
-                               'interpol_method',
+                               'interp_method',
                                'factor',
                                'lon_start',
                                'lon_end',
                                'lat_start',
                                'lat_end',
-                               'region_type']
-        self.data_type_values = {'gridname': str,
-                                 'fieldname_code': str,
-                                 'fieldname_file': str,
+                               'type']
+        self.data_type_values = {'grid_name': str,
+                                 'fieldname_in_model': str,
+                                 'fieldname_in_file': str,
                                  'file_name': str,
-                                 'interpol_method': str,
+                                 'interp_method': str,
                                  'factor': float,
                                  'lon_start': float,
                                  'lon_end': float,
                                  'lat_start': float,
                                  'lat_end': float,
-                                 'region_type': str}
+                                 'type': str}
 
         self.data_table_content = []
 
@@ -95,6 +113,7 @@ class DataType:
         for iline in self.data_table_content:
             iline_count += 1
             if iline.strip() != '' and '#' not in iline.strip()[0]:
+                self.verboseprint("---> Working on line:" + iline.replace("\n", ""))
                 # get rid of comment at the end of line
                 iline_list = iline.split('#')[0].split(',')
                 try:
@@ -116,66 +135,68 @@ class DataType:
                     raise TableParseError(self.data_table_file,
                                           iline_count,
                                           iline)
-                # If the fieldname_file is empty (i.e no interpolation just
-                # multiplying by a constant), remove fieldname_file,
-                # file_name, and interpol_method
-                if (tmp_list['fieldname_file'] == ""):
-                    del tmp_list['fieldname_file']
-                    del tmp_list['file_name']
-                    del tmp_list['interpol_method']
-                self.data_type['data_table'].append(tmp_list)
+                data_table_entry = reformat_yaml(tmp_list)
+                self.verboseprint("Yaml for this line:")
+                self.verboseprint(yaml.dump(data_table_entry, sort_keys=False))
+                self.data_type['data_table'].append(data_table_entry)
 
     def read_and_parse_data_table(self):
         """Open, read, and parse the legacy ascii data_table file"""
         if self.data_table_content != []:
             self.data_table_content = []
+
+        self.verboseprint("Opening and reading the data_table:" + self.data_table_file)
         self.read_data_table()
+
+        self.verboseprint("Parsing the data_table:" + self.data_table_file)
         self.parse_data_table()
 
     def convert_data_table(self):
         """Convert the legacy ascii data_table file to yaml"""
         self.read_and_parse_data_table()
 
+        self.verboseprint("Writing the output yaml: " + self.yaml_table_file)
         with open(self.yaml_table_file, self.out_file_op) as myfile:
             yaml.dump(self.data_type, myfile, sort_keys=False)
 
 
-def main():
-    #: parse user input
-    parser = argparse.ArgumentParser(
-        prog='data_table_to_yaml',
-        description="Converts a legacy ascii data_table to a yaml data_table. \
-                     Requires pyyaml (https://pyyaml.org/) \
-                     More details on the data_table yaml format can be found \
-                     in \
-                     https://github.com/NOAA-GFDL/FMS/tree/main/data_override")
-    parser.add_argument('-f', '--in-file',
-                        dest='in_file',
-                        type=str,
-                        default="data_table",
-                        help='Name of the data_table file to convert')
-    parser.add_argument('-o', '--output',
-                        dest='out_file',
-                        type=str,
-                        default='data_table.yaml',
-                        help="Ouput file name of the converted YAML \
-                              (Default: 'data_table.yaml')")
-    parser.add_argument('-F', '--force',
-                        action='store_true',
-                        help="Overwrite the output data table yaml file.")
-    parser.add_argument('-V', '--version',
-                        action="version",
-                        version=f"%(prog)s {__version__}")
-    args = parser.parse_args()
+def reformat_yaml(tmp_list):
+    """Convert the dictionary as it was read in to the output yaml"""
+    data_table_entry = {}
+    data_table_entry['grid_name'] = tmp_list['grid_name']
+    data_table_entry['fieldname_in_model'] = tmp_list['fieldname_in_model']
+    data_table_entry['factor'] = tmp_list['factor']
+    if tmp_list['fieldname_in_file'] != "":
+        override_file = {}
+        override_file['file_name'] = tmp_list['file_name']
+        override_file['fieldname_in_file'] = tmp_list['fieldname_in_file']
+        override_file['interp_method'] = tmp_list['interp_method']
+        if ":" in tmp_list['file_name']:
+            multi_file = {}
+            file_name_str = tmp_list['file_name']
+            if file_name_str.count(':') != 2:
+                raise Exception("If using the multi-file capability. Three filename must be set, seperated by a comma.")
 
-    try:
-        test_class = DataType(data_table_file=args.in_file,
-                              yaml_table_file=args.out_file,
-                              force_write=args.force)
-        test_class.convert_data_table()
-    except Exception as err:
-        raise SystemExit(err)
+            file_names = file_name_str.split(":")
+            multi_file['prev_file_name'] = file_names[0]
+            multi_file['next_file_name'] = file_names[2]
+            override_file['file_name'] = file_names[1]
+            override_file['multi_file'] = multi_file
+        else:
+            override_file['file_name'] = tmp_list['file_name']
+
+        if 'type' in tmp_list:
+            subregion = {}
+            subregion['lon_start'] = tmp_list['lon_start']
+            subregion['lon_end'] = tmp_list['lon_end']
+            subregion['lat_start'] = tmp_list['lat_start']
+            subregion['lat_end'] = tmp_list['lat_end']
+            subregion['type'] = tmp_list['type']
+            data_table_entry['subregion'] = subregion
+        data_table_entry['override_file'] = override_file
+
+    return data_table_entry
 
 
 if __name__ == "__main__":
-    main()
+    data_to_yaml(prog_name="data_to_yaml")
