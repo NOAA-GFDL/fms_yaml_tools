8057d82c2b20ad81935c0d6ad21ac61d12cb6a40
Uriel Ramirez <uramirez8707@gmail.com>
2025-07-16 11:43:00 -0400

    Add licenses to new files

--- fms_yaml_tools/diag_table/test_constants.py
+++ fms_yaml_tools/diag_table/test_constants.py
@@ -1,3 +1,24 @@
+#!/usr/bin/env python3
+# ***********************************************************************
+# *                   GNU Lesser General Public License
+# *
+# * This file is part of the GFDL Flexible Modeling System (FMS) YAML
+# * tools.
+# *
+# * FMS_yaml_tools is free software: you can redistribute it and/or
+# * modify it under the terms of the GNU Lesser General Public License
+# * as published by the Free Software Foundation, either version 3 of the
+# * License, or (at your option) any later version.
+# *
+# * FMS_yaml_tools is distributed in the hope that it will be useful, but
+# * WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# * General Public License for more details.
+# *
+# * You should have received a copy of the GNU Lesser General Public
+# * License along with FMS.  If not, see <http://www.gnu.org/licenses/>.
+# ***********************************************************************
+
 # Constants for diag_table tests
 DUPLICATE_DIAG_FILE_SAME_YAML = {
     "title": "Very_Important_Title",
